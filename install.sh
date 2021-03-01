cf15d43543c1cb613fd42cf1550be4096358ee4d
Luke Tudge <luketudge@gmail.com>
2021-03-01 21:32:10 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,10 +9,8 @@ then
 fi
 
 # Use requirements.txt just for the testing environment.
-# Make this reproducible by pinning all versions.
 pip install -r requirements.txt
 
 # Use setup.py for actual package dependencies.
-# Don't pin these.
 python setup.py bdist_wheel
 pip install --force-reinstall dist/spoonerize*.whl
